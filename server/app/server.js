1d213af8ec530d357c959d8cc475236f3d7b609b
Ruben-JR <rubenpina758@gmail.com>
2023-12-11 20:13:51 -0100

    add db connection

--- server/app/server.js
+++ server/app/server.js
@@ -1,25 +1,45 @@
 const mongoose = require("mongoose");
 
-const config = require("./config");
+const config = require("./config/config");
 const app = require("./app");
 
-class Database {
-    constructor() {
-        this._connect();
-    }
 
-    _connect() {
-        mongoose.connect(config.mongo_uri).then(() => {
-            console.log("Connection ok");
-        }).catch((err) => {
-            console.error("Erro to connect", err);
-        });
+// NOTE: connect to db
+mongoose.connect(config.mongo_uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => {
+		console.log('* CONNECTED TO DB')
+	})
+	.catch((error) => {
+		console.error('Error connecting to MongoDB:', error)
+	})
 
-        const server = app.listen(config.port, () => {
-            console.log("Server running on", config.port);
-        });
-    }
+const server = app.listen(config.port, () => {
+	console.log('* SERVER RUNNING ON', config.port)
+})
 
-}
+const io = socket(server, {
+	cors: {
+		origin: [
+			'http://localhost:3000',
+		]
+	}
+})
 
-module.exports = new Database();
+io.on('connection', (socket) => {
+	socket.on('send-message', (message, chat_id) => {
+		socket.to(chat_id).emit('receive-message', message)
+	})
+
+	socket.on('join-chat', (chat_id) => {
+		if (socket.lastRoom) {
+			socket.leave(socket.lastRoom)
+			socket.lastRoom = null
+		}
+
+		socket.join(chat_id)
+		socket.lastRoom = chat_id
+	})
+})
