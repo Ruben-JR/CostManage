f4b295b840560e1692a3ae52c8c50b7512c960b0
Ruben-JR <rubenpina758@gmail.com>
2023-12-18 21:47:20 -0100

    footer and toolbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
-import Login from "./pages/auth/login";
-import Register from "./pages/auth/register";
-
 function App() {
   return (
-    <div className="flex justify-center">
-      <Login />
-      <Register />
+    <div>
     </div>
   );
 }
