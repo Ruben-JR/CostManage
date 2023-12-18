cdda91a81ea8f7f8344c7cd0d271f69417e3407a
Ruben-JR <rubenpina758@gmail.com>
2023-12-18 09:10:11 -0100

    user authentication done

--- apiGateway/keycloak/keycloak_schema.py
+++ apiGateway/keycloak/keycloak_schema.py
@@ -5,5 +5,20 @@ class loginSchema(BaseModel):
     password: str
 
 class registerSchema(BaseModel):
+    userName: str
     firstName: str
     lastName: str
+    email: str
+    password: str
+    phone: int
+
+class userUpdateSchema(BaseModel):
+    userName: str
+    firstName: str
+    lastName: str
+    password: str
+    phone: str
+
+class changePasswordSchema(BaseModel):
+    old_password: str
+    new_password: str
