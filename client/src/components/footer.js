317cbe1aaddc7f7420ba92cd284c6c0a18039a47
Ruben-JR <75695011+Ruben-JR@users.noreply.github.com>
2023-12-19 10:12:27 -0100

    Login/regiter (#21)
    * create login and register page
    * create reset password and profile

---
+++ client/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="bg-white shadow dark:bg-gray-800">
+            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/Ruben-JR" class="hover:underline">Ruben de Pina</a>.
+                </span>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ client/src/components/sidenav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidenav = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Sidenav;
