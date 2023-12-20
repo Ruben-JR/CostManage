e793d3715db0944488476e6dab68dea8f849a595
Ruben-JR <75695011+Ruben-JR@users.noreply.github.com>
2023-12-20 10:09:45 -0100

    modal created (#23)

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer class="bg-white shadow bg-gray-50 dark:bg-gray-800 mt-auto">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
-                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/Ruben-JR" class="hover:underline">Ruben de Pina</a>.</span>
+        <footer className="bg-white shadow bg-gray-50 dark:bg-gray-800 mt-auto">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/Ruben-JR" class="hover:underline">Ruben de Pina</a>.</span>
             </div>
         </footer>
     );
--- client/src/pages/auth/profile.js
+++ client/src/pages/auth/profile.js
@@ -4,7 +4,7 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 const Profile = () => {
     return (
         <div className="my-12 sm:w-full">
-            <form class="bg-white rounded-lg m-14">
+            <form className="bg-white rounded-lg m-14">
                 <div className="space-y-12">
                     <div className="border-b border-gray-900/10 pb-12">
                         <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
