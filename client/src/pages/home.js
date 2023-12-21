18331432c00e3d0971b435f01af4cb944bf96472
Ruben-JR <75695011+Ruben-JR@users.noreply.github.com>
2023-12-21 09:47:41 -0100

    Home  (#24)
    * home page done

---
+++ client/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Charts from '../components/shared/charts';
+
+const Home = () => {
+
+    return (
+        <div>
+            <Charts />
+        </div >
+    );
+}
+
+export default Home;
