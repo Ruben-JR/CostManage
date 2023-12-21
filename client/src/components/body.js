cbea49f0f4c2bf91edd12f31a547d4ca0cca3639
Ruben-JR <rubenpina758@gmail.com>
2023-12-21 12:37:46 -0100

    funcionarios page done

--- client/src/components/body.js
+++ client/src/components/body.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Home from "../pages/home";
+import Funcionarios from '../pages/funcionarios';
 
 
 const Body = () => {
     return (
         <div className="p-14 sm:ml-56 mt-12">
-            <Home />
+            {/* <Home /> */}
+            <Funcionarios />
         </div>
     );
 }
