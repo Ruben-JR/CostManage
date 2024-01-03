d6004a54ad1a42175d463665dec31c376685a1dc
Ruben-JR <75695011+Ruben-JR@users.noreply.github.com>
2024-01-03 16:27:33 -0100

    empresa method http done (#35)

--- server/app/app.js
+++ server/app/app.js
@@ -7,6 +7,7 @@ const projetos = require("./routes/projetos")
 const tag = require("./routes/tag")
 const tipo_gasto = require("./routes/tipo_gastos")
 const transacao = require("./routes/transacao")
+const empresa = require("./routes/empresa")
 
 // Express app
 const app = express();
@@ -26,5 +27,6 @@ app.use("/projetos", projetos)
 app.use("/tag", tag)
 app.use("/tipo-gastos", tipo_gasto)
 app.use("/transacao", transacao)
+app.use("/empresa", empresa)
 
 module.exports = app
